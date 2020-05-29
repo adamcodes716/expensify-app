2c71f88e453394b3b561cd47122e197d9f97636a
adamcodes716 <adamcodes716@gmail.com>
2020-05-29 14:06:07 -0400

    87 Commit

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,14 +1,21 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = { count: 0}, action) => {   
+const store = createStore((state = { count: 0}, action) => {  
+    console.log (action.type); 
  switch (action.type) {
     case 'INCREMENT': 
-       return {
-           count: state.count + 1
+    const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
+       return {           
+           count: state.count + incrementBy
        };
        case 'DECREMENT': 
+       const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
        return {
-           count: state.count - 1
+           count: state.count - decrementBy
+       };
+       case 'SET': 
+       return {
+           count: action.count
        };
        case 'RESET': 
        return {
@@ -21,19 +28,28 @@ const store = createStore((state = { count: 0}, action) => {
    
 } );
 
-console.log(store.getState());
+const unsubscribe = store.subscribe(() => {
+    console.log(store.getState());  // prints state every time it changes
+});
 
 //increment the count
 store.dispatch ({
-    type: 'INCREMENT'
+    type: 'INCREMENT',
+    incrementBy: 5
 });
 
 //decrement the count
 store.dispatch ({
-    type: 'DECREMENT'
+    type: 'DECREMENT',
+    decrementBy: 10
 });
 
 //RESET the count
 store.dispatch ({
     type: 'RESET'
+});
+
+store.dispatch ({
+    type: 'SET',
+    count: 101
 });
