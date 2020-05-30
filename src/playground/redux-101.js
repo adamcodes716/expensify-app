f0f2b801fe3c30adab9928d612acf61748c35033
adamcodes716 <adamcodes716@gmail.com>
2020-05-30 11:38:37 -0400

    91 commit

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,12 +1,36 @@
 import { createStore } from 'redux';
 
+// Action generators - functions tha return action objects
+
+
+const incrementCount = ({incrementBy = 1} = {}) => ({
+    type: 'INCREMENT',
+    incrementBy
+});
+
+const decrementCount =  ({decrementBy = 1} = {}) => ({
+    type: 'DECREMENT',
+    decrementBy
+});
+
+const setCount =  ({count } = {}) => ({
+    type: 'SET',
+    count
+});
+
+const resetCount = () => ({
+    type: 'RESET'
+});
+
+
+
+
 const store = createStore((state = { count: 0}, action) => {  
     console.log (action.type); 
  switch (action.type) {
     case 'INCREMENT': 
-    const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
-       return {           
-           count: state.count + incrementBy
+        return {           
+           count: state.count + action.incrementBy
        };
        case 'DECREMENT': 
        const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
@@ -33,23 +57,12 @@ const unsubscribe = store.subscribe(() => {
 });
 
 //increment the count
-store.dispatch ({
-    type: 'INCREMENT',
-    incrementBy: 5
-});
+store.dispatch (incrementCount({incrementBy: 5}));
 
 //decrement the count
-store.dispatch ({
-    type: 'DECREMENT',
-    decrementBy: 10
-});
+store.dispatch (decrementCount({decrementBy: 10}));
 
 //RESET the count
-store.dispatch ({
-    type: 'RESET'
-});
+store.dispatch (resetCount());
 
-store.dispatch ({
-    type: 'SET',
-    count: 101
-});
+store.dispatch (setCount({count: 101}));
