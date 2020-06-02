ea7f86ca98edf9158abd25fbc749b9d0dc31489c
adamcodes716 <adamcodes716@gmail.com>
2020-06-02 08:29:15 -0400

    Commit 101

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
@@ -9,14 +10,25 @@ import getVisibleExpenses from './selectors/expenses'; // this is default export
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-const store=configureStore();
+const store = configureStore();
 
 store.dispatch(addExpense({ description: 'Water Bill', amount: 100, createdAt: -21000}));
 store.dispatch(addExpense({ description: 'Gas Bill', amount: 100, createdAt: -22000}));
-store.dispatch(setTextFilter('bill'));
+store.dispatch(setTextFilter('water'));
+
+setTimeout(() => {
+    store.dispatch(setTextFilter('gas'));
+}, 3000)
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);;
+console.log(visibleExpenses);
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider> 
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
+// ReactDOM.render(<AppRouter />, document.getElementById('app'));
---
+++ src/components/ExpenseList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { connect }  from 'react-redux';
+
+const ExpenseList = (props) => (
+    <div>
+        <h1>Expense List</h1>
+        {props.filters.text}
+        {props.expenses.length}
+    </div>
+);
+
+const mapStateToProps = (state) => {
+    // function to tell the store what to allow us to access
+    return {
+        expenses: state.expenses,
+        filters: state.filters
+    }
+};
+
+//const ConnectedExpenseList = connect((state) => {
+    export default connect(mapStateToProps)(ExpenseList);
+
+// export default ConnectedExpenseList;
