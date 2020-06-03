93b588693fb06957f14a3e0f24640c82180e6154
adamcodes716 <adamcodes716@gmail.com>
2020-06-03 10:15:11 -0400

    108 Commit

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import ExpenseForm from './ExpenseForm';
+import { editExpense, removeExpense } from '../actions/expenses';
 
 const EditExpensePage= (props) => {
-    return ( <div>
-       Editing the expense with ID of {props.match.params.id}
+    return ( 
+    <div>
+       <ExpenseForm
+         expense={props.expense}
+          onSubmit={(expense) => {
+              //Dispatch the action to edit the expense
+              props.dispatch(editExpense(props.expense.id, expense));
+              //Redirect to the dashboard
+              props.history.push('/'); // will use routing and then not full refresh
+            console.log('updated', expense);
+        } } 
+       />
+       <button onClick= {() => {
+         props.dispatch(removeExpense({ id: props.expense.id })); // shorthane
+         props.history.push('/'); // will use routing and then not full refresh
+        }}
+        >Remove</button>
     </div>
     );  
-}
+};
 
-export default EditExpensePage;
+const mapStateToProps = (state, props) => {
+   return {
+       expense: state.expenses.find((expense) => { // could also convert this to shorthand
+           return expense.id === props.match.params.id;
+       })
+   }
+};
+
+export default connect(mapStateToProps)(EditExpensePage);
+//export default EditExpensePage;
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,16 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { removeExpense } from '../actions/expenses';
 
 const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
     <div>
+        <Link to={`/edit/${id}`} >
         <h3>{description}</h3>
-        <p>{amount} - {createdAt}</p>
-        <button onClick= {() => {
-            dispatch(removeExpense({ id: id })) // could use shorthand here
-        }}
-        >Remove</button>
-    </div>
+        </Link> 
+          <p>{amount} - {createdAt}</p>
+   </div>
 );
 
-export default connect()(ExpenseListItem);
+export default ExpenseListItem;
