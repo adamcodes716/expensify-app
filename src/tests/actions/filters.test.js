b9417bb1d7f5598ae1a9322baa8223eb08c3b7d3
adamcodes716 <adamcodes716@gmail.com>
2020-06-03 15:13:15 -0400

    commit 114

--- src/tests/actions/filters.test.js
+++ src/tests/actions/filters.test.js
@@ -1,9 +1,50 @@
-import { addExpense, editExpense, removeExpense } from '../../actions/expenses';
+import { setStartDate, setEndDate, setTextFilter, sortByAmount, sortByDate } from '../../actions/filters';
+import moment from 'moment';
 
-test ('this shold setup remove expense action object', () => {
-    const action = removeExpense({id: '123abc'});
+test ('this should generate set start date action object', () => {
+    const action = setStartDate(moment(0));
     expect(action).toEqual({
-        type: 'REMOVE_EXPENSE',
-        id: '123abc'
+        type: 'SET_START_DATE',
+        startDate: moment(0)
+    });
+});
+
+test ('this should generate set end date action object', () => {
+    const action = setEndDate(moment(0));
+    expect(action).toEqual({
+        type: 'SET_END_DATE',
+        endDate: moment(0)
+    });
+});
+
+// set text filter - two tests
+test ('this should generate set text filter with value passed in', () => {
+    const text = 'Hello';
+    const action = setTextFilter(text);
+    expect(action).toEqual({
+        type: 'SET_TEXT_FILTER',
+        text: text
+    });
+});
+
+test ('this should generate set text filter with NO value passed in', () => {
+    const action = setTextFilter('');
+    expect(action).toEqual({
+        type: 'SET_TEXT_FILTER',
+        text: ''
+    });
+});
+
+// set sort by filters (2)
+test ('this should generate set text filter with sort by amount', () => {
+    const action = sortByAmount();
+    expect(action).toEqual({
+        type: 'SORT_BY_AMOUNT'
+    });
+});
+test ('this should generate set text filter with sort by date', () => {
+    const action = sortByDate();
+    expect(action).toEqual({
+        type: 'SORT_BY_DATE'
     });
 });
