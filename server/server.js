1b189793db70572931e338493283f76b066f8c99
adamcodes716 <adamcodes716@gmail.com>
2020-06-08 09:33:45 -0400

    Setup production build and server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express'); // node way of including libraries
 const app = express();  // we now have an express app
 const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.port || 3000; // if no port use 3000
 
 app.use(express.static(publicPath)); // we will take the return value from express (a function);
 
@@ -9,7 +10,7 @@ app.get('*', (req, res) => { //necessary to handle routing issues
    res.sendFile(path.join(publicPath, 'index.html'));  
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('server is up');    // callback function when server is running
 });
 
