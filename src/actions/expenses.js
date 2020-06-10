2cfeb04e9954bf740ebd8fe55fd8ea29fe21505c
adamcodes716 <adamcodes716@gmail.com>
2020-06-10 14:49:47 -0400

    152 Async redux actions

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,23 +1,43 @@
 import uuid from 'uuid';
+import database from '../firebase/firebase';
+
+// component calls action generator
+// action generator returns an object
+// component dispatches object
+// redux store changes
+
+// after update: 
+// component calls action generator
+// action generator returns a function
+// component dispatches function (react does not allow this directly)
+// function runs (has the ability to dispatch other actions and whateever it wants)
 
 // ADD_EXPENSE
-export const addExpense = (
-  {
-    description = '',
-    note = '',
-    amount = 0,
-    createdAt = 0
-  } = {}
-) => ({
-  type: 'ADD_EXPENSE',
-  expense: {
-    id: uuid(),
-    description,
-    note,
-    amount,
-    createdAt
-  }
+export const addExpense = (expense) => ({
+    type: 'ADD_EXPENSE',
+    expense
 });
+
+// new addExpense write to firebase
+export const startAddExpense = (expenseData = {}) => { // pass in expense if it exists
+  return (dispatch) => {
+    const { // destructure from expenseDAta
+      description = '',  // setting defaults if they don't exist
+      note = '',
+      amount = 0,
+      createdAt = 0
+    } = expenseData;  // destructuring from this
+    const expense = {description, note, amount, createdAt};  // cleaner code
+    database.ref('expenses').push(expense).then((ref) => {  // saving the data
+      //dispatch action from above so redux store changes
+      //the success 'then' gets called with a reference that we can use below
+      dispatch(addExpense({
+        id: ref.key,
+        ...expense
+      }));
+    });
+  };
+};
 
 // REMOVE_EXPENSE
 export const removeExpense = ({ id } = {}) => ({
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -15,30 +15,153 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-database.ref().set({
+export { firebase, database as default }; // make this usable anywhere
+
+/* // child_removed - a new event
+database.ref('expenses').on('child_removed', (snapshot) => { // a different event
+    console.log(snapshot.key, snapshot.val());
+});
+
+// child_changed
+database.ref('expenses').on('child_changed', (snapshot) => { // a different event
+    console.log(snapshot.key, snapshot.val());
+});
+
+// child_added
+database.ref('expenses').on('child_added', (snapshot) => { // a different event
+    console.log(snapshot.key, snapshot.val());
+}); */
+
+/* 
+
+ const onValueChange = database.ref().on('value',  (snapshot) => {
+    const expenses = [];
+
+        snapshot.forEach((childSnapshot) => {
+            expenses.push({
+                id: childSnapshot.key, // array requires an id
+                ...childSnapshot.val() // spread out the rest of the object
+            });
+        });
+        console.log(expenses)
+}, (e) => {
+    console.log('Error with data fetching', e);
+}); */
+
+/* database.ref('expenses')
+    .once('value')
+    .then((snapshot) => {
+        const expenses = [];
+
+        snapshot.forEach((childSnapshot) => {
+            expenses.push({
+                id: childSnapshot.key, // array requires an id
+                ...childSnapshot.val()
+            });
+        });
+        console.log(expenses)
+    });
+    */
+
+/*  database.ref('expenses').push({
+    description: 'First expenses!',
+    note:  'first note',
+    amount: 346.97,
+    createdAt: 1000
+}); */ 
+
+// database.ref('notes/-M9TrJbBicjUcChF4i4G').remove();
+
+/* database.ref('notes/-M9TrJbBicjUcChF4i4G').update({
+    body:  'This is a new body'
+}); */
+
+/* database.ref('notes').push({
+    title: 'Second Note!',
+    body:  'This is another note'
+}); */
+
+/* const firebaseNotes = {
+    notes : {
+        1234: {
+            title: 'First NOte!',
+            body:  'This is my note'
+        },
+        1235: {
+            title: 'Another NOte!',
+            body:  'This is my note'
+        }
+    }
+}; */
+
+/* database.ref().on('value', (snapshot) => { 
+    const val = snapshot.val();
+    console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
+})
+.catch((e) => {
+    console.log('Error fetching data', e);
+});  */
+
+
+
+/* const onValueChange = database.ref().on('value',  (snapshot) => {
+    console.log(snapshot.val());
+}, (e) => {
+    console.log('Error with data fetching', e);
+});
+
+setTimeout(() => {
+    database.ref('age').set(55);
+}, 3500);
+
+setTimeout(() => {
+    database.ref().off(onValueChange);
+}, 7000);
+
+setTimeout(() => {
+    database.ref('age').set(65);
+}, 10500); */
+
+/* database.ref()
+.once('value')
+.then((snapshot) => {  // snapshot is return value
+    const val = snapshot.val();
+    console.log(val);
+})
+.catch((e) => {
+    console.log('Error fetching data', e);
+}); */
+
+/* database.ref().set({
     name:'Adam Morgan',
-    age: 46,
-    isSingle: false,
-    location : {
+   age: 46,
+   stressLevel: 6,
+   job: {
+        title: 'Software Developer',
+        company: 'Google'
+   }, 
+   location : {
         city: 'Grand Island',
         country: 'UnitedStates'
-    }
-}).then(() => {
+   }
+ }).then(() => {
     console.log ('Data is saved');
-}).catch((e) => {
+ }).catch((e) => {
     console.log ('this failed');
-});
+ });
 
-// database.ref('age').set(47);
-// database.ref('location/city').set('Western New York');
+database.ref('isSingle').remove()
+    .then(() => {
+        console.log('removed isSingle');
+    })
+    .catch(() => {
+        console.log('error removing isSingle');
+    });
 
-database.ref('attributes').set({
-    height: 71,
-    weight: 200
-}).then(() => {
-    console.log('attributes are set');
-}).catch((e) => {
-    console.log('attributes failed');
-});
+    database.ref().update({
+        stressLevel: 7,
+        'location/city': 'Seattle'
+    }); */
+
 
 
