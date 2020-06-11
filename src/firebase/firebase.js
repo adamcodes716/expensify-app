63347347b8a582b5e87b8647e957ce72d222e3ec
adamcodes716 <adamcodes716@gmail.com>
2020-06-11 11:34:45 -0400

    156 Heroku vars

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,14 @@
 import * as firebase from 'firebase'; 
 
 const config = {
-    apiKey: "AIzaSyDMdbDvxy9ezdCtUf0UkQgBoh86M6jSdkc",
-    authDomain: "expensify-ac366.firebaseapp.com",
-    databaseURL: "https://expensify-ac366.firebaseio.com",
-    projectId: "expensify-ac366",
-    storageBucket: "expensify-ac366.appspot.com",
-    messagingSenderId: "696131513943",
-    appId: "1:696131513943:web:cff9ce1497701e88043d12",
-    measurementId: "G-1P0WS6J3DF"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+   //  appId: "1:696131513943:web:cff9ce1497701e88043d12",
+  //  measurementId: "G-1P0WS6J3DF"
 };
 
 firebase.initializeApp(config);
